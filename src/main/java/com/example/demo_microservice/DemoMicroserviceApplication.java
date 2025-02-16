941ee3d8b59a70eccce7581567b1d41895dd78d0
ipekimnci <ipekimanci@gmail.com>
2025-02-17 00:27:54 +0300

    application.yml added

--- src/main/java/com/example/demo_microservice/DemoMicroserviceApplication.java
+++ src/main/java/com/example/demo_microservice/DemoMicroserviceApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo_microservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Demo Management API", version = "1.0", description = "Demo Application"))
 public class DemoMicroserviceApplication {
 
 	public static void main(String[] args) {
